4dac07cdca0cae9730beb8f0d9a608998f898960
marciorenato <marcio.bullessich@gmail.com>
2022-11-04 00:59:46 +0000

    script.sh docker file

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# get all images that start with localhost:32000, output the results into image_ls file
+sudo microk8s ctr images ls name~='localhost:32000' | awk {'print $1'} > image_ls 
+# loop over file, remove each image
+cat image_ls | while read line || [[ -n $line ]];
+do
+    echo "$line"
+    microk8s ctr images rm $line
+done;
